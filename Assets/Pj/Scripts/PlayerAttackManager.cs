3378463ad48e245414bd24c96d2315e3763ae68b
sebaeiz24 <sebaeiz@gmail.com>
2025-09-27 17:43:22 -0300

    Animation Chocar

--- Assets/Pj/Scripts/PlayerAttackManager.cs
+++ Assets/Pj/Scripts/PlayerAttackManager.cs
@@ -5,12 +5,14 @@ public class PlayerAttackManager : MonoBehaviour
 {
     private PlayerController controller;
     private EnemyHealthSystem enemyHealth;
+    private Animator animator;
 
     [SerializeField] float dashDamage = 10f;
     [SerializeField] float airDropDamage = 10f;
     private void Start()
     {
         controller = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
         enemyHealth = GameObject.Find("Ymir").GetComponent<EnemyHealthSystem>();
 
     }
@@ -39,11 +41,13 @@ public class PlayerAttackManager : MonoBehaviour
 
     private void DashAttack()
     {
+        animator.SetTrigger("Choque");
         enemyHealth.TakeDamage(dashDamage);
     }
 
     private void AirDropAttack()
     {
+        animator.SetTrigger("Choque");
         //Lanzar el objeto y, si golpea en el enemigo, hacerle daño
     }
 }
